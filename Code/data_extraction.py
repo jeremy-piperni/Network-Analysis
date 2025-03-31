91052384e06bec6058f377469e8d4c08a785c394
jeremy-piperni <jeremypiperni@hotmail.com>
2025-03-31 01:07:50 -0400

    Finished data cleaning/exportation of all network data, Project Proposal updated

--- Code/data_extraction.py
+++ Code/data_extraction.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 import genre_mapping
+import re
 
 # Get the dataset
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -35,14 +36,68 @@ def map_words(text):
     mapped_words = [genre_mapping.map_genre(word) for word in words]
     return ', '.join(mapped_words)
 
+# Function to format the author names properly
+def format_author(author):
+    author = author.lower().strip()
+    author = author.encode('ascii', 'ignore').decode()
+    if ' ' not in author:
+        return author
+    else:
+        author = re.split(r',?\s+', author) 
+        if len(author) == 2:
+            if len(author[0]) < len(author[1]):
+                return f"{author[0]} {author[1]}"
+            else:
+                return f"{author[1]} {author[0]}"
+        if len(author) == 3:
+            if author[2].endswith('.'):
+                if author[1].endswith('.'):
+                    return f"{author[1]} {author[2]} {author[0]}"
+                else:
+                    return f"{author[2]} {author[0]} {author[1]}"
+    
+    return ' '.join(author)
+
+# Function to format descriptions
+def format_description(description):
+    description = description.lower().strip()
+    description = description.encode('ascii', 'ignore').decode()
+    return description
+
 # Get the mapped genres, remove old genre column
 df["Mapped Genres"] = df["Genre"].apply(lambda x: map_words(x))
 df = df.drop(columns=["Genre"])
 
-# Export dataset1, Title and Genre
+# Clean the author column
+df["Author"] = df["Author"].astype(str).apply(format_author)
+
+# Clean the description column
+df["Description"] = df["Description"].astype(str).apply(format_description)
+
+# Export Genre-Book network data
 df1 = df.drop(columns=["Author","Description","Banned"])
 script_dir1 = os.path.dirname(os.path.abspath(__file__))
-new_path1 = os.path.join(script_dir1, os.pardir, "Data", "final_dataset1.csv")
+new_path1 = os.path.join(script_dir1, os.pardir, "Data", "genre_book_network.csv")
 new_path1 = os.path.abspath(new_path1)
 df1.to_csv(new_path1, index=False)
 
+# Export Author-Book network data
+df2 = df.drop(columns=["Mapped Genres","Description","Banned"])
+script_dir2 = os.path.dirname(os.path.abspath(__file__))
+new_path2 = os.path.join(script_dir2, os.pardir, "Data", "author_book_network.csv")
+new_path2 = os.path.abspath(new_path2)
+df2.to_csv(new_path2, index=False)
+
+# Export Author-Genre network data
+df3 = df.drop(columns=["Title","Description","Banned"])
+script_dir3 = os.path.dirname(os.path.abspath(__file__))
+new_path3 = os.path.join(script_dir3, os.pardir, "Data", "author_genre_network.csv")
+new_path3 = os.path.abspath(new_path3)
+df3.to_csv(new_path3, index=False)
+
+# Export Book-Description network data
+df4 = df.drop(columns=["Mapped Genres","Author","Banned"])
+script_dir4 = os.path.dirname(os.path.abspath(__file__))
+new_path4 = os.path.join(script_dir4, os.pardir, "Data", "book_description_network.csv")
+new_path4 = os.path.abspath(new_path4)
+df4.to_csv(new_path4, index=False)
